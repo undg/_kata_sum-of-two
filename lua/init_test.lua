06d26a3321a913feba55dfb4e145f71d92e6a18d
undg <und3rdg@gmail.com>
2026-04-16 22:12:55 +0100

    up

--- lua/init_test.lua
+++ lua/init_test.lua
@@ -1,11 +1,12 @@
 local function two_sum(nums, target)
-    return nil
+	return nil
 end
 
-describe("two_sum", function()
-    it("finds two numbers that sum to target", function()
-        assert.are.same({0, 1}, two_sum({1, 2, 3}, 3))
-        assert.are.same({0, 2}, two_sum({1, 3, 2}, 3))
-        assert.are.same({1, 3}, two_sum({30, 15, 10, 15, 30}, 30))
-    end)
+describe('two_sum', function()
+	it('finds two numbers that sum to target', function()
+		assert.are.same({ 0, 1 }, two_sum({ 1, 2, 3 }, 3))
+		assert.are.same({ 0, 2 }, two_sum({ 1, 3, 2 }, 3))
+		assert.are.same({ 1, 3 }, two_sum({ 30, 15, 10, 15, 30 }, 30))
+		assert.are.same('not found', two_sum({ 30, 14, 10, 15, 30 }, 30))
+	end)
 end)
