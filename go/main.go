dae43e1bfaf7b2e26953822759945a3e1340515f
undg <und3rdg@gmail.com>
2026-04-17 00:34:19 +0100

    up

--- go/main.go
+++ go/main.go
@@ -7,7 +7,7 @@ import (
 var ErrNotFound = errors.New("not found")
 
 // O(n*n)
-func twoSum(nums []int, target int) ([]int, error) {
+func twoSumSkip(nums []int, target int) ([]int, error) {
 	for i, n := range nums {
 		tailIndexStart := i + 1
 		tail := nums[tailIndexStart:]
@@ -15,6 +15,19 @@ func twoSum(nums []int, target int) ([]int, error) {
 			if target == n+nn {
 				return []int{i, tailIndexStart + ii}, nil
 			}
+		}
+	}
+	return nil, ErrNotFound
+}
+
+func twoSum(nums []int, target int) ([]int, error) {
+	hashTable := make(map[int]int)
+	for i, n := range nums {
+		ii, ok := hashTable[target-n]
+		if ok {
+			return []int{ii, i}, nil
+		} else {
+			hashTable[n] = i
 		}
 	}
 	return nil, ErrNotFound
