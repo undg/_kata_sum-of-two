0a55a3e9bdd64fa2dc9be6a8a934fdd9f81161c3
undg <und3rdg@gmail.com>
2026-04-17 08:25:38 +0100

    up

--- lua/init.lua
+++ lua/init.lua
@@ -7,7 +7,14 @@ local M = {}
 ---@return number[]|nil, string|nil
 function M.two_sum(nums, target)
 	for i, n in ipairs(nums) do
+		for ii = i + 1, #nums do
+			local nn = nums[ii]
+			if target == n + nn then
+				return { i - 1, ii - 1 }, nil
+			end
+		end
 	end
+
 	return nil, 'not found'
 end
 
