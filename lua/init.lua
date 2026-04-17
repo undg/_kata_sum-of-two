a3376ae8a2904432a141d7951f681022f0ace14c
undg <und3rdg@gmail.com>
2026-04-17 08:37:16 +0100

    O(n)

--- lua/init.lua
+++ lua/init.lua
@@ -1,11 +1,11 @@
 local M = {}
 
--- O(n²)
+--- O(n²)
 ---
 ---@param nums number[]
 ---@param target number
 ---@return number[]|nil, string|nil
-function M.two_sum(nums, target)
+function M.two_sum_skip(nums, target)
 	for i, n in ipairs(nums) do
 		for ii = i + 1, #nums do
 			local nn = nums[ii]
@@ -13,6 +13,23 @@ function M.two_sum(nums, target)
 				return { i - 1, ii - 1 }, nil
 			end
 		end
+	end
+
+	return nil, 'not found'
+end
+
+--- O(n)
+---
+---@param nums number[]
+---@param target number
+---@return number[]|nil, string|nil
+function M.two_sum(nums, target)
+	local hash_map = {}
+	for i, n in ipairs(nums) do
+		if hash_map[target - n] ~= nil then
+			return { hash_map[target - n], i - 1 }, nil
+		end
+		hash_map[n] = i - 1
 	end
 
 	return nil, 'not found'
