602a044dfc54efb224d9c2fe82f3e174b1130daa
undg <und3rdg@gmail.com>
2026-04-17 08:15:24 +0100

    up

--- lua/init.lua
+++ lua/init.lua
@@ -1,6 +1,14 @@
 local M = {}
+
+-- O(n²)
+---
+---@param nums number[]
+---@param target number
+---@return number[]|nil, string|nil
 function M.two_sum(nums, target)
-	return 'not found'
+	for i, n in ipairs(nums) do
+	end
+	return nil, 'not found'
 end
 
 return M
